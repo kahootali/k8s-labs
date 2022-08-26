8dfe00478dc7fd24b6e37745301d797c529cc0c7
kahootali <kahoot.ali@gmail.com>
2022-08-27 04:20:11 +0500

    update manifests for hpa and mongo helm chart

--- module-3/mongo-helm/mongo-commands.sh
+++ module-3/mongo-helm/mongo-commands.sh
@@ -1,11 +1,15 @@
-helm template mongo-release bitnami/mongodb -n default -f mongo-values.yaml > mongo.yaml
-helm install mongo-release bitnami/mongodb -n default -f mongo-values.yaml  
+helm template mongo-release bitnami/mongodb --version 11.0.6 -n default -f mongo-values.yaml > mongo-templatized.yaml
+helm install mongo-release bitnami/mongodb --version 11.0.6 -n default -f mongo-values.yaml  
 
-mongo admin --host "mongo-release-mongodb-0.mongo-release-mongodb-headless.default.svc.cluster.local:27017,mongo-release-mongodb-1.mongo-release-mongodb-headless.default.svc.cluster.local:27017" --authenticationDatabase admin -u root -p root123
+# Now mongo DB server is running, for client side, we will be deploying another mongo pod, which will serve as client
+kubectl apply -f mongo-client.yaml 
 
-mongo admin --host "mongo-release-mongodb-1.mongo-release-mongodb-headless.default.svc.cluster.local:27017" --authenticationDatabase admin -u root -p root123
+mongosh admin --host "mongo-release-mongodb-0.mongo-release-mongodb-headless.default.svc.cluster.local:27017,mongo-release-mongodb-1.mongo-release-mongodb-headless.default.svc.cluster.local:27017" --authenticationDatabase admin -u root -p root123
 
-mongo admin --host "mongo-release-mongodb-0.mongo-release-mongodb-headless.default.svc.cluster.local:27017" --authenticationDatabase admin -u root -p root123
+mongosh admin --host "mongo-release-mongodb-1.mongo-release-mongodb-headless.default.svc.cluster.local:27017" --authenticationDatabase admin -u root -p root123
+
+mongosh admin --host "mongo-release-mongodb-0.mongo-release-mongodb-headless.default.svc.cluster.local:27017" --authenticationDatabase admin -u root -p root123
+
 use inventory
 db.inventory.insertMany([
    { item: "journal", qty: 25, status: "A", size: { h: 14, w: 21, uom: "cm" }, tags: [ "blank", "red" ] },
